9c0fc12e2fe8bd39eb82135f3b97aab697f0225e
Stephen Barreto <stephen.barreto@intelex.com>
2024-04-01 20:33:07 -0500

    buttons ready

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,25 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-white text-black border border-slate-200 border-2 border-b-[4px] active:border-b-2 hover:bg-slate-100 text-slate-500",
+          "bg-white text-black border border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500",
+        primary:
+          "bg-sky-400 text-primary-foreground border-sky-500  border-b-4 active:border-b-0 hover:bg-sky-400/90",
+        primaryOutline: "bg-white text-sky-500 hover:bg-slate-100",
+        secondary:
+          "bg-green-500 text-primary-foreground border-green-600  border-b-4 active:border-b-0 hover:bg-green-500/90",
+        secondaryOutline: "bg-white text-green-500 hover:bg-slate-100",
+        danger:
+          "bg-rose-500 text-primary-foreground border-rose-600  border-b-4 active:border-b-0 hover:bg-rose-500/90",
+        dangerOutline: "bg-white text-rose-500 hover:bg-slate-100",
+        super:
+          "bg-indigo-500 text-primary-foreground border-indigo-600  border-b-4 active:border-b-0 hover:bg-indigo-500/90",
+        superOutline: "bg-white text-indigo-500 hover:bg-slate-100",
+        ghost:
+          "bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100",
+        sidebar:
+          "bg-transparent text-slate-500 border-transparent border-2 hover:bg-slate-100 transition-none",
+        sidebarOutline:
+          "bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none",
       },
       size: {
         default: "h-11 px-4 py-2",
@@ -24,7 +42,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
+  },
 );
 
 export interface ButtonProps
@@ -43,7 +61,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     );
-  }
+  },
 );
 Button.displayName = "Button";
 
