8dd4b07bd5742ef41969af1de777667c4af8244f
Stephen Barreto <stephen.barreto@intelex.com>
2024-05-03 21:44:27 -0500

    prettier

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import { Sidebar } from "@/components/sidebar";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const MainLayout = ({ children }: Props) => {
+  return (
+    <>
+      <Sidebar className="hidden lg:flex" />
+      <main className="h-full lg:pl-[256px] pt-[50px] lg:pt-0">
+        <div className="h-full bg-red-500">{children}</div>
+      </main>
+    </>
+  );
+};
+
+export default MainLayout;
