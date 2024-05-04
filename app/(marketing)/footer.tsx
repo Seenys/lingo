45e51840b2204dcb5472d70a25b484b5a63f5b50
Stephen Barreto <stephen.barreto@intelex.com>
2024-05-03 19:47:53 -0500

    footer and images

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
-    <footer className="hidden lg:block  items-center justify-between p-4 bg-gray-800 text-white">
-      Footer
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/hr.svg"
+            alt="Croatian Flag"
+            width={40}
+            height={30}
+            className="mr-4 rounded-md"
+          />
+          Croatian
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/fr.svg"
+            alt="French Flag"
+            width={40}
+            height={30}
+            className="mr-4 rounded-md"
+          />
+          French
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/es.svg"
+            alt="Spanish Flag"
+            width={40}
+            height={30}
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/jp.svg"
+            alt="Japanese Flag"
+            width={40}
+            height={30}
+            className="mr-4 rounded-md"
+          />
+          Japanese
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/it.svg"
+            alt="Italian Flag"
+            width={40}
+            height={30}
+            className="mr-4 rounded-md"
+          />
+          Italian
+        </Button>
+      </div>
     </footer>
   );
 };
