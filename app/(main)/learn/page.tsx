b59900580dede01ae1ea9c0aabafe1e8b63e2233
Stephen Barreto <stephen.barreto@intelex.com>
2024-06-19 18:17:29 -0500

    feat(learn): learn page Wrapper header and sections

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,13 +1,24 @@
-import FeedWrapper from '@/components/feed-wrapper';
-import { StickyWrapper } from '@/components/sticky-wrapper';
-import { Stick } from 'next/font/google';
 import React from 'react';
+// components
+import { StickyWrapper } from '@/components/sticky-wrapper';
+import FeedWrapper from '@/components/feed-wrapper';
+import { UserProgress } from '@/components/user-progress';
+import Header from './header';
 
 const LearnPage = () => {
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
-      <StickyWrapper>Learn</StickyWrapper>
-      <FeedWrapper>my feed</FeedWrapper>
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: 'Spanish', imageSrc: '/es.svg' }}
+          hearts={0}
+          points={0}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
     </div>
   );
 };
