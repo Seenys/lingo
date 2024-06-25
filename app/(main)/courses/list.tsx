4c7990966cdf9a69650e0bc04896ac6912dc682f
Stephen Barreto <stephen.barreto@intelex.com>
2024-06-25 14:05:56 -0500

    feat(courses): añadir componente Card y List para cursos

---
+++ app/(main)/courses/list.tsx
@@ -0,0 +1,28 @@
+'use client';
+// Dependencies
+import { courses } from '@/db/schema';
+// Components
+import Card from './card';
+
+type Props = {
+  courses: (typeof courses.$inferSelect)[];
+  activeCourseId: number;
+};
+
+export const List = ({ courses, activeCourseId }: Props) => {
+  return (
+    <div className="p-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
+      {courses.map((course) => (
+        <Card
+          key={course.id}
+          id={course.id}
+          title={course.title}
+          imageSrc={course.imageSrc}
+          onClick={() => console.log(`Course ${course.title} clicked`)}
+          active={course.id === activeCourseId}
+          disabled={false}
+        />
+      ))}
+    </div>
+  );
+};
---
+++ app/(main)/courses/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// Components
+import { List } from './list';
+// Queries & Database
+import { getCourses } from '@/db/queries';
+
+const CoursesPage = async () => {
+  const courses = await getCourses();
+  return (
+    <div className="h-full max-w-[912px] px-3 mx-auto">
+      <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
+      <List courses={courses} activeCourseId={1} />
+    </div>
+  );
+};
+
+export default CoursesPage;
